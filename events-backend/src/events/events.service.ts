0eff117d6951cc11dff2aad48b81c97b97e56a5b
Abhishek Keshri <iam2kabhishek@gmail.com>
2025-01-21 09:11:07 +0530

    feat: add event service

---
+++ events-backend/src/events/events.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Event } from './event.entity';
+import { CreateEventDto } from './dto/create-event.dto';
+
+@Injectable()
+export class EventsService {
+  constructor(
+    @InjectRepository(Event)
+    private eventsRepository: Repository<Event>,
+  ) {}
+
+  findAll(): Promise<Event[]> {
+    return this.eventsRepository.find();
+  }
+
+  findOne(id: number): Promise<Event> {
+    return this.eventsRepository.findOne({ where: { id } });
+  }
+
+  create(createEventDto: CreateEventDto): Promise<Event> {
+    const event = this.eventsRepository.create(createEventDto);
+    return this.eventsRepository.save(event);
+  }
+
+  async update(id: number, updateEventDto: CreateEventDto): Promise<Event> {
+    await this.eventsRepository.update(id, updateEventDto);
+    return this.findOne(id);
+  }
+
+  async remove(id: number): Promise<void> {
+    await this.eventsRepository.delete(id);
+  }
+}
