1bb3ee6893b3e743b557745bcab5ac5cb72fb733
Abhishek Keshri <iam2kabhishek@gmail.com>
2025-01-21 11:05:47 +0530

    feat: add ui border

--- events-frontend/src/App.tsx
+++ events-frontend/src/App.tsx
@@ -5,7 +5,7 @@ import {EventForm} from './components/EventForm';
 function App() {
     return (
         <Router>
-            <div className='container bg-gray-950'>
+            <div className='container bg-gray-800 border rounded-lg border-gray-50'>
                 <div className='container p-8'>
                     <Routes>
                         <Route path='/' element={<EventList />} />
