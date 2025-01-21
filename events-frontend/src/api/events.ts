e6512c2bdb5322aaf1faf9a1071b81cae80fda18
Abhishek Keshri <iam2kabhishek@gmail.com>
2025-01-21 10:27:51 +0530

    fix: use process url

--- events-frontend/src/api/events.ts
+++ events-frontend/src/api/events.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import {Event} from '../types/event';
 
-const API_URL = process.env.API_URL;
+const API_URL = 'http://localhost:3000';
 
 export const getEvents = async (): Promise<Event[]> => {
     const response = await axios.get(`${API_URL}/events`);
