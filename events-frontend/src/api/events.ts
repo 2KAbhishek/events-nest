fc08e23f39c46db8f4bfbec0b803ae82fc06386f
Abhishek Keshri <iam2kabhishek@gmail.com>
2025-01-21 09:28:33 +0530

    feat: add events api

---
+++ events-frontend/src/api/events.ts
@@ -0,0 +1,31 @@
+import axios from 'axios';
+import {Event} from '../types/event';
+
+const API_URL = process.env.API_URL;
+
+export const getEvents = async (): Promise<Event[]> => {
+    const response = await axios.get(`${API_URL}/events`);
+    return response.data;
+};
+
+export const getEvent = async (id: number): Promise<Event> => {
+    const response = await axios.get(`${API_URL}/events/${id}`);
+    return response.data;
+};
+
+export const createEvent = async (event: Omit<Event, 'id'>): Promise<Event> => {
+    const response = await axios.post(`${API_URL}/events`, event);
+    return response.data;
+};
+
+export const updateEvent = async (
+    id: number,
+    event: Omit<Event, 'id'>
+): Promise<Event> => {
+    const response = await axios.put(`${API_URL}/events/${id}`, event);
+    return response.data;
+};
+
+export const deleteEvent = async (id: number): Promise<void> => {
+    await axios.delete(`${API_URL}/events/${id}`);
+};
